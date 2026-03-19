0936c380c95389cc4933a9147e0d5ec3165c0d14
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2026-03-19 13:02:43 +0000

    Check proto.Marshal result

--- internal/otelprocesscontext/otelprocesscontext.go
+++ internal/otelprocesscontext/otelprocesscontext.go
@@ -8,11 +8,16 @@ package otelprocesscontext
 //go:generate ./proto/generate.sh
 
 import (
+	"fmt"
+
 	"google.golang.org/protobuf/proto"
 )
 
 // PublishProcessContext marshals pc and publishes it to the process context mapping.
 func PublishProcessContext(pc *ProcessContext) error {
-	b, _ := proto.Marshal(pc)
+	b, err := proto.Marshal(pc)
+	if err != nil {
+		return fmt.Errorf("failed to marshal process context: %w", err)
+	}
 	return CreateOtelProcessContextMapping(b)
 }
